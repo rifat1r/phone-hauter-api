24c92d9158ecd9a02cdc3a44cf6718fd115a5633
rifat1r <rifatul564@gmail.com>
2024-03-01 12:52:14 +0600

    completed

--- js/phone.js
+++ js/phone.js
@@ -1,8 +1,8 @@
-const loadPhone = async(searchText,isShowAll) =>{
+const loadPhone = async(searchText=22,isShowAll) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
+    // console.log(phones);
     displayPhones(phones,isShowAll);
 }
 
@@ -20,14 +20,14 @@ if(phones.length > 12 && !isShowAll){
 else{
     showAllContainer.classList.add('hidden');
 }
-console.log('show all',isShowAll)
+// console.log('show all',isShowAll)
 //display only first 12 phones if not show all
 if(!isShowAll){
   phones = phones.slice(0,12);
 }
 
 phones.forEach(phone => {
-    console.log(phone);
+    // console.log(phone);
     //1. create a div
     const phoneCard = document.createElement('div');
     phoneCard.classList = `card p-4 bg-base-100 shadow-xl`;
@@ -38,8 +38,8 @@ phones.forEach(phone => {
             <div class="card-body">
               <h2 class="card-title">${phone.phone_name}</h2>
               <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+              <div class="card-actions justify-center">
+                <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
               </div>
             </div>`;
             //appendchild
@@ -49,6 +49,27 @@ phones.forEach(phone => {
       toggleLoadingSpinner(false)
 }
 
+//show details 
+const handleShowDetail = async(id) =>{
+  //load single phone data
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  const data = await res.json();
+  const phone = data.data;
+  showPhoneDetails(phone)
+}
+const showPhoneDetails = (phone)=>{
+  console.log(phone);
+  const phoneName = document.getElementById('show-detail-phone-name')
+  phoneName.innerText = phone.name;
+  const showDetailContainer = document.getElementById('show-detail-container');
+  showDetailContainer.innerHTML = `
+  <img src="${phone.image}" alt="">
+  <p><sapn>Storage:</sapn>${phone?.mainFeatures?.storage}</p>
+  <p><sapn>GPS:</sapn>${phone?.others?.GPS || 'No GPS Available'}</p>
+  ` 
+  //show modal
+  show_detail_modal.showModal()
+}
 //handle search button
 const handleSearch = (isShowAll)=>{
   toggleLoadingSpinner(true);
@@ -72,4 +93,4 @@ const handleShowAll = () => {
   handleSearch(true);
 }
 
-// loadPhone()
+loadPhone()
